2294508c3cc007346262f585646c374a13fe7ac3
Facu <fabrirm15@gmail.com>
2022-10-12 11:08:43 -0600

    Procedures

--- Query.sql
+++ Query.sql
@@ -25,7 +25,7 @@ CREATE DATABASE "Proyecto"
 
     );
     create table gathering_Responsible(
-        gathering_Responsible_id int primary key,
+        gathering_Responsible_id serial primary key,
         name varchar(50) not NULL
     );
     create table site(
@@ -44,7 +44,7 @@ CREATE DATABASE "Proyecto"
         Specimen_ID int primary key,
         taxon_id int,
         gathering_id int REFERENCES gathering(gathering_id),
-        specimen_description varchar(50) not null,
+        specimen_description varchar(10000) not null,
         specimen_cost float not null 
     );
 
@@ -72,3 +72,63 @@ create table temp(
 --recibir datos de la tabla specimen de un csv
 
 
+create or replace procedure pr_insertar_Especimen(
+	pSpecimen_ID int,pTaxon_id int,pGathering_id int ,pGathering int,pSpecimen_description varchar,pSpecimen_cost float)
+	LANGUAGE 'plpgsql' as $body$DECLARE
+    repetido int;
+    BEGIN
+    select count(*) into repetido from Specimen where specimen_id=pSpecimen_ID;
+    if repetido =0 THEN
+    insert into specimen values(pSpecimen_ID,pTaxon_id,pGathering_id,pSpecimen_description,pSpecimen_cost);
+    end if;
+    END;$body$;
+
+create or replace procedure pr_insertar_Taxon(
+    pTaxon_id int,pKingdom_name varchar,pPhylum_division_name varchar,pClass_name varchar,pOrder_name varchar,pFamily_name varchar,pGenus_name varchar,pSpecies_name varchar,pScientific_name varchar)
+    LANGUAGE 'plpgsql' as $body$DECLARE
+    repetido int;
+    BEGIN
+    select count(*) into repetido from taxon where taxon_id=pTaxon_id;
+    if repetido =0 THEN
+    insert into taxon values(pTaxon_id,pKingdom_name,pPhylum_division_name,pClass_name,pOrder_name,pFamily_name,pGenus_name,pSpecies_name,pScientific_name);
+    end if;
+    END;$body$;
+
+
+    create or replace procedure pr_insertar_Gathering(
+    pGathering_id int,pGathering_date date,pGathering_responsible_id int,pSite_id int)
+    LANGUAGE 'plpgsql' as $body$DECLARE
+    repetido int;
+    BEGIN
+    select count(*) into repetido from gathering where gathering_id=pGathering_id;
+    if repetido =0 THEN
+    insert into gathering values(pGathering_id,pGathering_date,pGathering_responsible_id,pSite_id);
+    end if;
+    END;$body$;
+
+    
+	create or replace procedure pr_normalizar()
+    LANGUAGE 'plpgsql' as $body$DECLARE
+    inbio CURSOR FOR select * from temp;
+	idtemp int;
+    random int;
+	repetido int;
+    BEGIN
+    for i in inbio loop
+    call pr_insertar_taxon(i.taxon_id,i.kingdom_name,i.phylum_division_name,i.class_name,i.order_name,i.family_name,i.genus_name,i.species_name,i.scientific_name);
+    select count(*) into repetido from site where site_id=i.site_id;
+    if repetido =0 THEN
+	insert into site values(i.site_id,i.latitude,i.longitude,i.site_description);
+	end if;
+    insert into gathering_responsible(name) values(i.gathering_responsible);
+	select gathering_Responsible_id into idtemp from gathering_responsible where name=i.gathering_responsible;
+    call pr_insertar_gathering(idtemp,i.gathering_date,idtemp,i.site_id);
+    SELECT floor(random() * 10000 + 1) INTO random;
+    call pr_insertar_especimen(i.specimen_id,i.taxon_id,idtemp,random,i.specimen_description,random);
+    end loop;
+    END;$body$;
+call pr_normalizar();
+
+
+
+delete from temp where gathering_responsible is NULL;
